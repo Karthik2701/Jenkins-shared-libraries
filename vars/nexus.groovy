0a6f913db072028cae96d8198c696f256c4f36ec
Karthik2701 <102400241+Karthik2701@users.noreply.github.com>
2023-07-14 15:32:31 -0700

    Update nexus.groovy

--- vars/nexus.groovy
+++ vars/nexus.groovy
@@ -1,3 +1,3 @@
-def call () {
+def call() {
   nexusPublisher nexusInstanceId: '12345', nexusRepositoryId: 'maven-releases', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: '', filePath: '/var/lib/jenkins/workspace/samplepipeline/target/helloworld.war']], mavenCoordinate: [artifactId: 'hello-world-servlet-example', groupId: 'com.geekcap.vmturbo', packaging: 'war', version: '$BUILD_NUMBER']]]
 }
